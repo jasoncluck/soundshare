14667e8174191e20bb77fbe3bb6b7f57dacdc37d
Jason Cluck <jcluck@ubuntu.(none)>
2013-03-21 09:13:01 -0700

    add soundcloud gem and example

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'sqlite3'
 gem 'therubyracer'
 gem 'less-rails'
 gem 'twitter-bootstrap-rails'
-
+gem 'soundcloud'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,12 @@
 class RoomsController < ApplicationController
+  require 'soundcloud'
+  before_filter :soundcloud_init
 
-
+  def soundcloud_init
+    client = Soundcloud.new(:client_id => 'da1368916c4f898320127b2cd453d36f')
+    track_url = 'http://soundcloud.com/forss/flickermood'
+    @embed_info = client.get('/oembed', :url => track_url)
+  end
 
 
   # GET /rooms
